b104d223e081d76f2f84743873d090e0011f2255
BitEU <45499599+BitEU@users.noreply.github.com>
2025-11-19 16:56:01 -0500

    Fixed UNIVAC build.bat carat error

--- build.bat
+++ build.bat
@@ -114,7 +114,7 @@ if %ERRORLEVEL% EQU 0 (
     echo Output: toledo_atomchess_univac.exe
 
     REM Display file size
-    for %%A in (toledo_atomchess_univac.exe^) do (
+    for %%A in (toledo_atomchess_univac.exe) do (
         echo File size: %%~zA bytes
     )
     echo.
